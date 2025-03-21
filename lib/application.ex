d48b853302c116397824339dae3a86171c04d3a4
umer.harroon <umerharroon@umer.harroon>
2025-03-21 15:28:52 +0500

    fix: use dynamic PORT from ENV for Railway

--- lib/application.ex
+++ lib/application.ex
@@ -2,8 +2,10 @@ defmodule MyPlugApp.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4000")
+
     children = [
-      {Plug.Cowboy, scheme: :http, plug: MyPlugApp.Router, options: [port: 4000]}
+      {Plug.Cowboy, scheme: :http, plug: MyPlugApp.Router, options: [port: port]}
     ]
 
     opts = [strategy: :one_for_one, name: MyPlugApp.Supervisor]
